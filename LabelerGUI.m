9a4fbb9a1e1b77cedb39867dcbf5f1d60fb6b80e
Allen Lee <allenleetc@gmail.com>
2015-07-23 18:44:47 -0400

    xplatform testing all UI units to pixels

--- LabelerGUI.m
+++ LabelerGUI.m
@@ -22,10 +22,10 @@ function varargout = LabelerGUI(varargin)
 
 % Edit the above text to modify the response to help LarvaLabeler
 
-% Last Modified by GUIDE v2.5 22-Jul-2015 12:56:07
+% Last Modified by GUIDE v2.5 23-Jul-2015 18:43:07
 
 % Begin initialization code - DO NOT EDIT
-gui_Singleton = 1;
+gui_Singleton = 0;
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
                    'gui_OpeningFcn', @LabelerGUI_OpeningFcn, ...
