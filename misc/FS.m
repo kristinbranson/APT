8910dfbc280142a6e104a7de0156c400f0a932c1
Allen <allenleetc@gmail.com>
2016-01-29 11:51:19 -0500

    add

---
+++ misc/FS.m
@@ -0,0 +1,54 @@
+classdef FS
+  
+  properties (Constant)
+    % TRPAT = '(?<td>.*)@(?<tdnote>.*)@
+  end
+  methods (Static)
+    
+    function [type,info] = parsename(n)
+      toks = regexp(n,'@','split');
+      ntok = numel(toks);
+      switch ntok
+        case 3
+          type = 'tp';
+          info = cell2struct(toks(2:end),{'name' 'date'},2);
+        case 4
+          type = toks{1};
+          switch type 
+            case 'td'
+              info = cell2struct(toks(2:end),{'name' 'note' 'date'},2);
+            case 'tdI'
+              info = cell2struct(toks(2:end),{'tdname' 'note' 'date'},2);
+          end
+        case 7
+          type = 'tr';
+          info = cell2struct(toks,{'tdname' 'tdnote' 'tdIname' 'tdIvar' 'tpname' 'sha' 'date'},2);
+        otherwise
+          assert(false);
+      end
+      
+    end
+    
+    function n = formTrainedClassifierName(tdname,tdIname,tdIvar,tpname,sha)
+      [~,s_td] = FS.parsename(tdname);
+      [~,s_tdI] = FS.parsename(tdIname);
+      [~,s_tp] = FS.parsename(tpname);
+      
+      n = sprintf('%s@%s@%s@%s@%s@%s@%s',s_td.name,s_td.note,s_tdI.note,tdIvar,...
+        s_tp.name,sha,datestr(now,'mmddTHHMM'));
+    end
+    
+    function n = formTestResultsFolderName(trname,tdname,tdIname,tdIvar)
+      [~,s_tr] = FS.parsename(trname);
+      [~,s_td] = FS.parsename(tdname);
+      [~,s_tdI] = FS.parsename(tdIname);
+      
+      n = sprintf('%s@%s@%s@%s@%s__%s@%s@%s@%s__%s',...
+        s_tr.tdname,s_tr.tdnote,s_tr.tdIname,s_tr.tdIvar,s_tr.tpname,...
+        s_td.name,s_td.note,s_tdI.note,tdIvar,...
+        datestr(now,'mmddTHHMM'));      
+    end
+    
+  end
+  
+end
