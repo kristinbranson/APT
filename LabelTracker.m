a864781562b45a1059fc70745ef494c00771f702
Allen Lee <allenleetc@gmail.com>
2016-04-01 17:06:10 -0400

    WIP track button

--- LabelTracker.m
+++ LabelTracker.m
@@ -3,16 +3,46 @@ classdef LabelTracker < handle
   % classifier to predict/track labels on new images.
   
   properties    
-    lObj % (back)handle to Labeler object    
+    lObj % (back)handle to Labeler object
+    ax % axis for viewing tracking results
+    
+    hLCurrMovie; % listener to lObj.currMovie
+    hLCurrFrame; % listener to lObj.currFrame
   end
   
   methods
     
     function obj = LabelTracker(labelerObj)
-      obj.lObj = labelerObj;
+      obj.lObj = labelerObj;   
+      
+      axOver = axisOverlay(obj.lObj.gdata.axes_curr);
+      axOver.LineWidth = 2;
+      obj.ax = axOver;
+      
+      obj.hLCurrMovie = addlistener(labelerObj,'currMovie','PostSet',@(s,e)obj.newLabelerMovie());
+      obj.hLCurrFrame = addlistener(labelerObj,'currFrame','PostSet',@(s,e)obj.newLabelerFrame());
     end
     
+    function delete(obj)
+      if ~isempty(obj.hLCurrMovie)
+        delete(obj.hLCurrMovie);
+      end
+      if ~isempty(obj.hLCurrFrame)
+        delete(obj.hLCurrFrame);
+      end
+    end
+    
+  end
+  
+  methods 
+    
     function track(obj)
+    end
+    
+    function newLabelerFrame(obj)
+    end
+    
+    function newLabelerMovie(obj)
     end
     
   end
