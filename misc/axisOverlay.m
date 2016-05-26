b841d4ce05eae78d218122567f0700b1378a82e1
Allen Lee <al@leapscientific.com>
2016-05-26 18:16:31 -0400

    moved to apt

--- misc/axisOverlay.m
+++
@@ -1,9 +0,0 @@
-function ax1 = axisOverlay(ax0,ax1)
-
-if exist('ax1','var')==0
-  ax1 = axes('Parent',ax0.Parent,'Visible','off','HitTest','off','Color','none');
-end
-PROPS = {'XLim' 'YLim' 'YDir' 'Position' 'PlotBoxAspectRatio'};
-for p = PROPS,p=p{1}; %#ok<FXSET>
-  ax1.(p) = ax0.(p);
-end
