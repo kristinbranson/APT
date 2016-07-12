91f03928220268768207201478c8921fe6adec01
Allen Lee <allenleetc@gmail.com>
2016-07-12 09:52:10 -0400

    multiview calibrated for head

---
+++ user/CalRigSH.m
@@ -0,0 +1,64 @@
+classdef CalRigSH < CalRig
+  
+  properties
+    nviews = 2;
+    viewNames = {'side' 'front'};
+    viewSizes = [1024 1024;1024 1024]; % [nviews x 2]. viewSizes(iView,:) gives [nc nr] or [width height]
+  end
+  
+  properties (SetAccess=private)
+    kineData; % scalar struct; kinemat calibration data
+    kineDataFile; % string    
+  end
+    
+  methods
+    
+    function obj = CalRigSH
+      obj.setKineData('');
+    end
+    
+    function setKineData(obj,kdfile)
+      if isempty(kdfile)
+        obj.kineData = [];
+        obj.kineDataFile = '';
+      else
+        kd = load(kdfile);
+        kd = kd.data;
+        if ~all(isfield(kd,{'types' 'cal' 'kine'}))
+          error('CalRigSH:kine',...
+            'Unexpected contents in Kinemat calibration file: ''%s''',kdfile);
+        end
+        obj.kineData = kd;
+        obj.kineDataFile = kdfile;
+      end
+    end
+    
+    % iView1: view index for anchor point
+    % xy1: [2]. [x y] vector, cropped coords in iView1
+    % iViewEpi: view index for target view (where EpiLine will be drawn)
+    %
+    % xEPL,yEPL: epipolar line, cropped coords, iViewEpi
+    function [xEPL,yEPL] = computeEpiPolarLine(obj,iView1,xy1,iViewEpi)
+      kdata = obj.kineData;
+      dlt_side = kdata.cal.coeff.DLT_1;
+      dlt_front = kdata.cal.coeff.DLT_2;
+      vsz = obj.viewSizes(iViewEpi,:);
+      if iView1==1 && iViewEpi==2 %view1==side; viewEpi==front
+        dlt1 = dlt_side;
+        dlt2 = dlt_front;
+      elseif iView1==2 && iViewEpi==1
+        dlt1 = dlt_front;
+        dlt2 = dlt_side;
+      else
+        assert(false);
+      end
+      [xEPL,yEPL] = im_pt_2_im_line(xy1(1),xy1(2),dlt1,dlt2,[1 vsz(1) 1 vsz(2)],20);      
+    end
+    
+    function [xRCT,yRCT] = reconstruct(obj,iView1,xy1,iView2,xy2,iViewRct)
+      assert(false,'Unsupported');
+    end
+    
+  end
+
+end
---
+++ user/im_pt_2_im_line.m
@@ -0,0 +1,94 @@
+function [u_2, v_2] = im_pt_2_im_line( u_1, v_1, DLT_1, DLT_2, im_bndry, len )
+% IM_PT_2_IM_LINE: get line in image #2 corresponding to point (u_1, v_1)
+% in image #1
+%
+% DLT_1 = 11 DLT coefficients for camera 1
+% DLT_2 = 11 DLT coefficients for camera 2
+% im_bndry = [xmin xmax ymin ymax] for image #2
+% len = number of points generated along line
+
+[ params_x, params_y, params_z ] = dlt_2D_to_3D( DLT_1, u_1, v_1 );
+
+% Find line intersections with image bndry
+
+s_list = [];
+
+% intersection with min( u_2 ) and max( u_2 )
+[ numer_min, denom_min ] = u_intersect_func(  params_x, params_y, params_z, DLT_2, im_bndry(1)  );
+
+[ numer_max, denom_max ] = u_intersect_func(  params_x, params_y, params_z, DLT_2, im_bndry(2)  );
+
+if abs(denom_min) > abs(eps) & abs(denom_max) > abs(eps) 
+
+    s_list = [ s_list,  numer_min/denom_min, numer_max/denom_max ];
+    
+end
+
+% intersection with min( v_2 ) and max( v_2 )
+[ numer_min, denom_min ] = v_intersect_func(  params_x, params_y, params_z, DLT_2, im_bndry(3)  );
+
+[ numer_max, denom_max ] = v_intersect_func(  params_x, params_y, params_z, DLT_2, im_bndry(4)  );
+
+if abs(denom_min) > abs(eps) & abs(denom_max) > abs(eps)
+
+    s_list = [ s_list,  numer_min/denom_min, numer_max/denom_max ];
+    
+end
+
+% No intersection with image bndry
+if isempty( s_list )
+    
+    u_2 = [];
+    
+    v_2 = [];
+   
+else
+    
+    s_max = max( s_list );
+    
+    s_min = min( s_list );
+    
+    s = linspace( s_min, s_max, len );
+    
+    x = params_x(1) + params_x(2)*s;
+    
+    y = params_y(1) + params_y(2)*s;
+    
+    z = params_z(1) + params_z(2)*s;
+   
+    [ u_2, v_2 ] = dlt_3D_to_2D( DLT_2, x, y, z );
+    
+end
+
+% -------------------------------------------------------------------------
+function [ numer, denom ] = u_intersect_func(  params_x, params_y, params_z, DLT, val )
+% INTERSECT_FUNC:  get numerator and denominator of interesction point
+
+% numer = params_x(1)*( -val*DLT(9) + DLT(1) )  + params_y(1)*( -val*DLT(10) + DLT(2) ) + ...
+%     params_z(1)*( -val*DLT(11) + DLT(3) ) + DLT(4) - val;
+% 
+% denom = params_x(2)*(  DLT(1) - val*DLT(9) )  + params_y(2)*( DLT(2) - val*DLT(10)) + ...
+%     params_z(2)*( DLT(3) - val*DLT(11) );
+
+numer = params_x(1)*( -val*DLT(9) + DLT(1) )  + params_y(1)*( -val*DLT(10) + DLT(2) ) + ...
+    params_z(1)*( -val*DLT(11) + DLT(3) ) + DLT(4) - val;
+
+denom = params_x(2)*(  -DLT(1) + val*DLT(9) )  + params_y(2)*( -DLT(2) + val*DLT(10)) + ...
+    params_z(2)*( -DLT(3) + val*DLT(11) );
+
+
+% -------------------------------------------------------------------------
+function [ numer, denom ] = v_intersect_func(  params_x, params_y, params_z, DLT, val )
+% INTERSECT_FUNC:  get numerator and denominator of interesction point
+
+% numer = params_x(1)*( -val*DLT(9) + DLT(5) )  + params_y(1)*( -val*DLT(10) + DLT(6) ) + ...
+%     params_z(1)*( -val*DLT(11) + DLT(7) ) + DLT(8) - val;
+% 
+% denom = params_x(2)*(  DLT(5) - val*DLT(9) )  + params_y(2)*( DLT(6) - val*DLT(10)) + ...
+%     params_z(2)*( DLT(7) - val*DLT(11) );
+
+numer = params_x(1)*( -val*DLT(9) + DLT(5) )  + params_y(1)*( -val*DLT(10) + DLT(6) ) + ...
+    params_z(1)*( -val*DLT(11) + DLT(7) ) + DLT(8) - val;
+
+denom = params_x(2)*(  -DLT(5) + val*DLT(9) )  + params_y(2)*( -DLT(6) + val*DLT(10)) + ...
+    params_z(2)*( -DLT(7) + val*DLT(11) );
