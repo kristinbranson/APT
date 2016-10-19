5c0080144ce377eca2a4c2d7fca01d11aef3d00e
Allen Lee <allenleetc@gmail.com>
2016-10-19 09:25:37 -0400

    pruning in APTCluster

--- APTCluster.m
+++ APTCluster.m
@@ -9,9 +9,69 @@ function APTCluster(varargin)
 % % Options passed to Labeler.trackAndExport, 'trackArgs'
 % APTCluster(lblFile,'track',moviefullpath,varargin)
 %
+% % Prune legs of a trkfile, saving results alongside trkfile
+% APTCluster(0,'prunejan',trkfullpath,sigd)
 
 lblFile = varargin{1};
 action = varargin{2};
+
+if isequal(lblFile,'0') || isequal(lblFile,0)
+  % NON-LBLFILE ACTIONS
+  
+  switch action
+    case 'prunejan'
+      trkfile = varargin{3};
+      if exist(trkfile,'file')==0
+        error('APTCluster:file','Cannot find file: ''%s''.',trkfile);
+      end
+      sigd = varargin{4};
+      if ~isnumeric(sigd)
+        sigd = str2double(sigd);
+      end
+      
+      assert(~verLessThan('matlab','R2016b'),'''prunejan'' requires MATLAB R2016b or later.');
+      
+      trk = load(trkfile,'-mat');
+      trkPFull = trk.pTrkFull;
+      [npttrk,d,nRep,nfrm] = size(trkPFull);
+      trkD = npttrk*d;
+      trkPFull = reshape(trkPFull,[trkD nRep nfrm]);
+      trkPFull = permute(trkPFull,[3 2 1]);
+      trkPiPt = trk.pTrkiPt;
+      assert(numel(trkPiPt)==npttrk);
+
+      IMNR = 256;
+      IMNC = 256;
+      LEGS = 4:7;      
+      NLEG = numel(LEGS);
+      pLegsPruned = nan(NLEG,2,nfrm);
+      pLegsPrunedAbs = nan(NLEG,2,nfrm);
+      for iLeg = 1:NLEG
+        ipt = LEGS(iLeg);
+        pObj = CPRPrune(IMNR,IMNC,sigd);
+        pObj.init(trkPFull,trkPiPt,ipt,1,nfrm);
+        pObj.run();
+        pLegsPruned(iLeg,:,:) = pObj.prnTrk';
+        pLegsPrunedAbs(iLeg,:,:) = pObj.prnTrkAbs';
+      end
+      trkPruned = TrkFile(pLegsPruned,'pTrkiPt',LEGS);
+      trkPrunedAbs = TrkFile(pLegsPrunedAbs,'pTrkiPt',LEGS);
+      
+      [trkfileP,trkfileS] = fileparts(trkfile);
+      trkfilePruned = fullfile(trkfileP,[trkfileS '_legsPruned.trk']);
+      trkfilePrunedAbs = fullfile(trkfileP,[trkfileS '_legsPrunedAbs.trk']);
+      
+      fprintf(1,'Saving: %s...\n',trkfilePruned);
+      trkPruned.save(trkfilePruned);
+      fprintf(1,'Saving: %s...\n',trkfilePrunedAbs);
+      trkPrunedAbs.save(trkfilePrunedAbs);            
+    otherwise
+      assert(false,'Unrecognized action.');
+  end
+  
+  return;
+end
+ 
 if exist(lblFile,'file')==0
   error('APTCluster:file','Cannot find project file: ''%s''.',lblFile);
 end
