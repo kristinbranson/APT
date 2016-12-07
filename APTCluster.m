f7cc93cd6eb0893e0fde4c58217da549a262e822
Allen Lee <allenleetc@gmail.com>
2016-12-07 09:50:18 -0500

    pruning cleanup

--- APTCluster.m
+++ APTCluster.m
@@ -56,7 +56,7 @@ if isequal(lblFile,'0') || isequal(lblFile,0)
     lambda = varargin{8};
     if ~isnumeric(lambda)
       lambda = str2double(lambda);
-  end  
+    end  
   end
   
   assert(~verLessThan('matlab','R2016a'),...
@@ -85,7 +85,7 @@ if isequal(lblFile,'0') || isequal(lblFile,0)
     case 'prunerf2'
       pObj = CPRPruneGen(IMNR,IMNC,sigd,lambda);
     otherwise
-  pObj = CPRPrune(IMNR,IMNC,sigd);
+      pObj = CPRPrune(IMNR,IMNC,sigd);
   end
   pObj.init(trkPFull,trkPiPt,ipt,frmstart,frmend);
   pObj.run();
@@ -96,8 +96,16 @@ if isequal(lblFile,'0') || isequal(lblFile,0)
   trkPrunedAbs = TrkFile(pLegsPrunedAbs,'pTrkiPt',ipt,'pTrkFrm',frmstart:frmend);
   
   [trkfileP,trkfileS] = fileparts(trkfile);
-  filebase = sprintf('_prune%02d_sig%02d_%d_%d.trk',ipt,round(sigd),frmstart,frmend);
-  filebaseAbs = sprintf('_pruneAbs%02d_sig%02d_%d_%d.trk',ipt,round(sigd),frmstart,frmend);
+  switch action
+    case 'prunerf2'
+      filebase = sprintf('_prune%02d_sig%02d_lam%.3f_%d_%d.trk',ipt,round(sigd),lambda,frmstart,frmend);
+      filebaseAbs = sprintf('_pruneAbs%02d_sig%02d_lam%.3f_%d_%d.trk',ipt,round(sigd),lambda,frmstart,frmend);
+      filebaseObj = sprintf('_pruneObj%02d_sig%02d_lam%.3f_%d_%d.trk',ipt,round(sigd),lambda,frmstart,frmend);
+    otherwise
+      filebase = sprintf('_prune%02d_sig%02d_%d_%d.trk',ipt,round(sigd),frmstart,frmend);
+      filebaseAbs = sprintf('_pruneAbs%02d_sig%02d_%d_%d.trk',ipt,round(sigd),frmstart,frmend);
+      filebaseObj = sprintf('_pruneObj%02d_sig%02d_%d_%d.trk',ipt,round(sigd),frmstart,frmend);
+  end
   trkfilePruned = fullfile(trkfileP,[trkfileS filebase]);
   trkfilePrunedAbs = fullfile(trkfileP,[trkfileS filebaseAbs]);
   objFile = fullfile(trkfileP,[trkfileS filebaseObj]);
