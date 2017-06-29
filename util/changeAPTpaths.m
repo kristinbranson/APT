66d2fc8fb6cdcc4f8fb792a4e615b9f7e218b0dc
stephenHuston <stephenhuston@gmail.com>
2017-06-29 14:37:18 -0400

    Removed hard-coded path

--- util/changeAPTpaths.m
+++ util/changeAPTpaths.m
@@ -1,28 +1,35 @@
-function changeAPTpaths(oldpathSegment2replace, newpathSegment)
+function changeAPTpaths(oldpathSegment2replace, newpathSegment, dataroot)
 %changes all paths in a project if videos have been moved around
 %
 % oldpathSegment2replace = part of path string that is wrong, only has to
 % be part of the path not whole path
 %
 % newpathSegment = string to replace oldpathSegment2replace with
+%
+% dataroot = Used if you would like to use path macros in your project's
+% video paths
+% (https://github.com/kristinbranson/APT/wiki/Path-Macros).  Set this to the
+% part of path that will be replaced with a path macro e.g. dataroot = 'Z:'
+% if your paths all start with Z: and you would like this to be replaced 
+% with the $dataroot macro.  Set to empty (dataroot=[]) otherwise.
 
 [fname,path]=uigetfile(['*.lbl'],'Select APT project to replace path for');
 
 
-
-dataroot =  [hustonlab,'\']; %root of paths, usually path to hustonlab AKA 'Z:'
-
 loadFname = [path,fname];
 
-%replace this with saveFname = loadFname when working
 saveFname =loadFname;
 
 lbl = load(loadFname,'-mat');
 
 oldPaths = lbl.movieFilesAll;
 
-%replacing part of path with data root
-pathsMinusDataRoot = regexprep(oldPaths,dataroot,'$dataroot');
+%replacing dataroot part of path with $dataroot macro
+if ~isempty(dataroot)
+    pathsMinusDataRoot = regexprep(oldPaths,dataroot,'$dataroot');
+else
+    pathsMinusDataRoot = oldPaths;
+end
 
 %replacing text segment
 newPaths = regexprep(pathsMinusDataRoot,oldpathSegment2replace,newpathSegment);
