cf7b919b04d298a685c5e5e9a0277088ba9d1ad1
Allen Lee <allenleetc@gmail.com>
2017-10-31 12:22:25 -0400

    re #117 perf optim, add return args

--- user/+pxfern/compfeatures.m
+++ user/+pxfern/compfeatures.m
@@ -1,4 +1,4 @@
-function [fvals,fvalsc] = compfeatures(prm,tGT,fimidxs)
+function [fvals,fmd] = compfeatures(prm,tGT,fimidxs)
 % tGT: MFT table with 
 %  * imForeCanonRoi: [roinr roinc] single, bgsubed foreground px
 %  * imForeCanonRoiBWLleg: [roinr roinc] uint8, label vector labeling
@@ -7,7 +7,7 @@ function [fvals,fvalsc] = compfeatures(prm,tGT,fimidxs)
 %
 % fvals: [nFern x fernD x nFgPx] computed feature values
 %   (diff-of-px-intensities) at all foreground px found/considered
-% fvalsc: [nFgPx x 1] class/leg labeling 3rd dim of fvals
+% fmd: [nFgPx x 1] metadata table labeling 3rd dim of fvals
 
 %ctrIdx = (nptsFtrSqrROI+1)/2;
 %ctrIdx = nan; % we are not using
@@ -20,7 +20,13 @@ ftrBoxSz = 2*ftrRad+1;
 nGT = height(tGT);
 
 fvals = nan(nFern,fernD,0); % acc of all feature vals
-fvalsc = nan(0,1); % legs labeling 3rd dim of ftrValsBig
+%fvalsc = nan(0,1); % acc of legs labeling 3rd dim of ftrValsBig
+
+% acc of MD labeling 3rd dim of ftrValsBig. 
+iGTacc = nan(0,1); % row index into tGT
+legacc = nan(0,1);
+iacc = nan(0,1);
+jacc = nan(0,1); % row/col indices into .imForeCanonBWLleg for pixel in question
 for iGT=1:nGT
   if mod(iGT,100)==0
     disp(iGT);
@@ -35,10 +41,11 @@ for iGT=1:nGT
     for ilegPt=1:nlegPt
       i = legPtI(ilegPt);
       j = legPtJ(ilegPt);
-      [imFtrBox,npadl,npadu] = padgrab(imFC,0,i-ftrRad,i+ftrRad,...
-        j-ftrRad,j+ftrRad);
-      assert(all(npadl==0));
-      assert(all(npadu==0));
+      imFtrBox = imFC(i-ftrRad:i+ftrRad,j-ftrRad:j+ftrRad); % perf skip the padgrab
+%       [imFtrBox,npadl,npadu] = padgrab(imFC,0,i-ftrRad,i+ftrRad,...
+%         j-ftrRad,j+ftrRad);
+%       assert(all(npadl==0));
+%       assert(all(npadu==0));
       szassert(imFtrBox,[ftrBoxSz ftrBoxSz]);
       
       ftrVals = imFtrBox(fimidxs);
@@ -48,7 +55,13 @@ for iGT=1:nGT
       %ftrVals = ftrCtr-ftrVals;      
       
       fvals(:,:,end+1) = ftrVals; %#ok<AGROW>
-      fvalsc(end+1,1) = leg; %#ok<AGROW>
+      %fvalsc(end+1,1) = leg; %#ok<AGROW>
+      iGTacc(end+1,1) = iGT; %#ok<AGROW>
+      legacc(end+1,1) = leg; %#ok<AGROW>
+      iacc(end+1,1) = i; %#ok<AGROW>
+      jacc(end+1,1) = j; %#ok<AGROW>
     end
   end
 end
+
+fmd = table(iGTacc,legacc,iacc,jacc,'VariableNames',{'iGT','leg','i','j'});
--- user/+pxfern/xv.m
+++ user/+pxfern/xv.m
@@ -1,10 +1,11 @@
-function [cvpart,C,corrPct] = xv(prm,tGT,kfold)
+function [cvpart,C,corrPct,tblRes] = xv(prm,tGT,kfold)
 
 tblfldscontainsassert(tGT,MFTable.FLDSID);
 
 cvpart = cvpartition(tGT.mov,'KFold',kfold);
 C = cell(kfold,1);
 corrPct = cell(kfold,1);
+tblRes = cell(kfold,1);
 
 hWB = waitbar(0,sprintf('Fold 0/%d',kfold));
 for ifold=1:kfold
@@ -14,22 +15,31 @@ for ifold=1:kfold
   
   tfTrn = cvpart.training(ifold);
   tblTrn = tGT(tfTrn,:);
-  [fvalsTrn,fvalscTrn] = pxfern.compfeatures(prm,tblTrn,fimidxs);
-  hfernTrn = pxfern.fernhist(prm,fvalsTrn,fvalscTrn);
+  [fvalsTrn,fmdTrn] = pxfern.compfeatures(prm,tblTrn,fimidxs);
+  hfernTrn = pxfern.fernhist(prm,fvalsTrn,fmdTrn.leg);
   pfernTrn = pxfern.fernprob(prm,hfernTrn);
   
   tfTst = cvpart.test(ifold);
   tblTst = tGT(tfTst,:);
-  [fvalsTst,fvalscTst] = pxfern.compfeatures(prm,tblTst,fimidxs);
+  [fvalsTst,fmdTst] = pxfern.compfeatures(prm,tblTst,fimidxs);
   
   fvalscTstPred = pxfern.fernpred(prm,pfernTrn,fvalsTst);
-  c = confusionmat(fvalscTst,fvalscTstPred,'order',1:6);
+  c = confusionmat(fmdTst.leg,fvalscTstPred,'order',1:6);
   cZ = sum(c,2);
-  cnorm = c./cZ;
-  corrpcts = diag(cnorm)
+  cnorm = c./cZ;  
+  corrpcts = diag(cnorm);  
   
   C{ifold} = c;
   corrPct{ifold} = corrpcts;
+  tblRes{ifold} = [...
+    fmdTst(:,{'leg' 'i' 'j'}) ...
+    tblTst(fmdTst.iGT,{'mov' 'frm' 'iTgt' 'p' 'pTrx'...
+                      'trxa' 'trxb' 'trxth' 'pcanon' 'idxLposLegCanon'}) ...
+    table(fvalscTstPred,repmat(ifold,height(fmdTst),1),'VariableNames',...
+          {'legpred','ifold'}) ...
+  ];  
 end
+
+tblRes = cat(1,tblRes{:});
 
 delete(hWB);
