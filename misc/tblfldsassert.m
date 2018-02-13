68a8fff52be40a965880bdf747b9f360de5758b2
Allen Lee <allenleetc@gmail.com>
2018-02-13 11:14:43 -0500

    cover for 2014b questionable table behavior

--- misc/tblfldsassert.m
+++ misc/tblfldsassert.m
@@ -3,10 +3,11 @@ function tblfldsassert(t,flds,varargin)
 enforceorder = myparse(varargin,...
   'enforceorder',false);
 
+varNames = t.Properties.VariableNames;
+varNames = varNames(:); % AL20180213: columnizing must be separate from indexing in 2014b
 if enforceorder
-  assert(isequal(t.Properties.VariableNames(:),flds(:)),...
-    'Table fields do not match specification.');
+  assert(isequal(varNames,flds(:)),'Table fields do not match specification.');
 else
-  assert(isequal(sort(t.Properties.VariableNames(:)),sort(flds(:))),...
+  assert(isequal(sort(varNames),sort(flds(:))),...
     'Table fields do not match specification.');
 end
