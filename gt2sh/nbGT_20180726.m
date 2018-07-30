184f28c333dbe793494ddb31d1b63bda7f6846c3
Allen Lee <allenleetc@gmail.com>
2018-07-30 09:31:00 -0400

    sh gt head angle anls cont'ued

--- gt2sh/nbGT_20180726.m
+++ gt2sh/nbGT_20180726.m
@@ -560,10 +560,98 @@ fprintf(1,'Removing %d intra rows that had no match. Left with %d intra row-pair
   nnz(tfrm),nIntra);
 
 
+%% Compute head-angle-err on intra rows
+
+% fly2calib = readtable(FLYNUM2CALIB,...
+%   'Delimiter',',',...
+%   'ReadVariableNames',false,...
+%   'HeaderLines',0);
+% fly2calib.Properties.VariableNames = {'fly' 'calibfile'};
+% fly2calibMap = containers.Map(fly2calib.fly,fly2calib.calibfile);
+
+intra_axisAngRadMag_dLbl = nan(nIntra,1);
+intra_axisAngRadMag_dLbl_nopt2 = nan(nIntra,1);
+intra_axAngDegXYZMag = nan(nIntra,1);
+intra_daxAngDegXYZMag = nan(nIntra,1);
+intra_l2err_dLbl = nan(nIntra,1);
+for iIntra=1:nIntra
+  iGT = ijIntra(iIntra,1);
+  jGT = ijIntra(iIntra,2);
+  
+  mIdxI = tGT.mIdx(iGT);
+  mIdxJ = tGT.mIdx(jGT);
+  iMovI = abs(mIdxI);
+  iMovJ = abs(mIdxJ);
+  crObjI = lbl.viewCalibrationDataGT{iMovI};
+  crObjJ = lbl.viewCalibrationDataGT{iMovJ};
+  
+  assert(tGT.frm(iGT)==tGT.frm(jGT));  
+  frm = tGT.frm(iGT);
+
+  pLblI = tGT.pLbl(iGT,:);
+  pLblJ = tGT.pLbl(jGT,:);
+  
+  tfDLT = isa(crObjI,'CalRigSH');
+  if tfDLT
+    dltstuff = crObjI.kineData.cal.coeff;
+    assert(isequal(crObjJ.kineData.cal.coeff,dltstuff));
+    assert(all(isfield(dltstuff,{'DLT_1';'DLT_2'})));
+  else
+    dltstuff = [];
+    assert(isequal(crObjI.rvecs,crObjJ.rvecs));
+  end
+  
+  % check pLbl
+  xyLblI = reshape(pLblI,10,2);
+  xyLblJ = reshape(pLblJ,10,2);
+  uvLblI = cat(3,xyLblI(1:5,:),xyLblI(6:10,:)); % [5x2x2]. pt,(x/y),vw
+  uvLblJ = cat(3,xyLblJ(1:5,:),xyLblJ(6:10,:)); % [5x2x2]. pt,(x/y),vw  
+  szassert(uvLblI,[5 2 2]);
+  szassert(uvLblJ,[5 2 2]);
+  
+  % 3dize
+  X3DlblI = nan(5,3); % pt,(x/y/z)
+  X3DlblJ = nan(5,3);
+%   X3DlblStroErr = nan(1,5);
+%   X3DtrkStroErr = nan(1,5);
+  for ipt=1:5
+    if tfDLT
+      A = [dltstuff.DLT_1,dltstuff.DLT_2];
+      tempxyzblah = reconfu(A,[uvLblI(ipt,:,1) uvLblI(ipt,:,2)]);
+      X3DlblI(ipt,:) = tempxyzblah(1:3);      
+      tempxyzblah = reconfu(A,[uvLblJ(ipt,:,1) uvLblJ(ipt,:,2)]);
+      X3DlblJ(ipt,:) = tempxyzblah(1:3);      
+    else % orthocam
+      X3DlblI(ipt,:) = ...
+        crObjI.stereoTriangulate(uvLblI(ipt,:,1)',uvLblI(ipt,:,2)'); % [1x3]
+      X3DlblJ(ipt,:) = ...
+        crObjJ.stereoTriangulate(uvLblJ(ipt,:,1)',uvLblJ(ipt,:,2)'); % [1x3] crObjI/J should be same
+    end
+  end
+
+  [~,~,~,~,~,axisAngRad] = computeRotations(X3DlblI,X3DlblJ);
+  assert(numel(axisAngRad)==7);
+  idxno2 = [1 3 4 5];
+  [~,~,~,~,~,axisAngRad_nopt2] = ...
+    computeRotations(X3DlblI(idxno2,:),X3DlblJ(idxno2,:));  
+    
+  intra_axisAngRadMag_dLbl(iIntra) = axisAngRad(end);
+  intra_axisAngRadMag_dLbl_nopt2(iIntra) = axisAngRad_nopt2(end);
+  
+  
+  axAngDegI = tGT.axAngDegXYZ(iGT,1);
+  axAngDegJ = tGT.axAngDegXYZ(jGT,1);
+  intra_axAngDegXYZMag(iIntra) = axAngDegI;
+  intra_daxAngDegXYZMag(iIntra) = axAngDegJ-axAngDegI;
+  pxerr = sqrt(sum((uvLblI-uvLblJ).^2,2)); % [5x1x2]. pt, ., vw
+  intra_l2err_dLbl(iIntra) = mean(pxerr(:));
+end
+
+
 %%
 
-DOSAVE = false;
-SAVEDIR = 'figsFull';
+DOSAVE = true;
+SAVEDIR = 'figsRT';
 
 tiLblArgs = {'fontweight','bold','interpreter','none','fontsize',20};
 axLblArgs = {'fontweight','bold','interpreter','none','fontsize',16};
@@ -572,14 +660,32 @@ hFig = [];
 
 hFig(end+1) = figure(15);
 hfig = hFig(end);
-tstr = 'axisAngRadMag inc. pt2 vs not';
+tstr = 'head ang err (trk vs lbl), inc. pt2 vs not. All GT rows';
 set(hfig,'Name',tstr,'Position',[2561 401 1920 1124]);
 clf;
 scatter(tGT.axisAngRadMag_dLblTrk/pi*180,tGT.axisAngRadMag_dLblTrk_nopt2/pi*180);
+hold on
+xl = xlim(gca);
+plot(xl,xl,'-r','linewidth',1);
 grid on;
 title(tstr,tiLblArgs{:});
 xlabel('angle (deg) inc pt2',axLblArgs{:});
 ylabel('angle (deg) no pt2',axLblArgs{:});
+
+hFig(end+1) = figure(17);
+hfig = hFig(end);
+tstr = 'head ang err (inc pt2) vs mean px err, trk vs lbl. All GT rows';
+set(hfig,'Name',tstr,'Position',[2561 401 1920 1124]);
+clf;
+scatter(tGT.meanL2err,tGT.axisAngRadMag_dLblTrk/pi*180);
+grid on;
+lims = axis;
+lims([1 3]) = 0;
+axis(lims);
+title(tstr,tiLblArgs{:});
+xlabel('mean L2 err (px)',axLblArgs{:});
+ylabel('angle err (deg) pt2',axLblArgs{:});
+
 
 tfNotIntra = ~strcmp(tGT.type,'intra');
 nNotIntra = nnz(tfNotIntra);
@@ -587,27 +693,141 @@ fprintf(1,'%d not-intra rows.\n',nNotIntra);
 
 hFig(end+1) = figure(20);
 hfig = hFig(end);
-tstr = sprintf('n (not intra)=%d. headAngErr vs headAng, inc. pt2',nNotIntra);
+tstr = sprintf('n (not intra)=%d. headAngErr vs headAng',nNotIntra);
 set(hfig,'Name',tstr,'Position',[2561 401 1920 1124]);
-clf;
+axs = mycreatesubplots(1,2,.1);
+linkaxes(axs);
+
+ax = axs(1);
+axes(ax);
+tstr = sprintf('n (not intra)=%d. headAngErr vs headAng, inc. pt2',nNotIntra);
 scatter(tGT.axAngDegXYZ(tfNotIntra,1),tGT.axisAngRadMag_dLblTrk(tfNotIntra)/pi*180);
 grid on;
 title(tstr,tiLblArgs{:});
-xlabel('head ang mag',axLblArgs{:});
+xlabel('head ang mag (deg)',axLblArgs{:});
 ylabel('head ang err (deg) inc pt2',axLblArgs{:});
 
-hFig(end+1) = figure(25);
-hfig = hFig(end);
-tstr = sprintf('n (not intra)=%d. headAngErr vs headAng, NO. pt2',nNotIntra);
-set(hfig,'Name',tstr,'Position',[2561 401 1920 1124]);
-clf;
+ax = axs(2);
+axes(ax);
+tstr = sprintf('NO pt2');
 scatter(tGT.axAngDegXYZ(tfNotIntra,1),tGT.axisAngRadMag_dLblTrk_nopt2(tfNotIntra)/pi*180);
 grid on;
 title(tstr,tiLblArgs{:});
-xlabel('head ang mag',axLblArgs{:});
+xlabel('head ang mag (deg)',axLblArgs{:});
 ylabel('head ang err (deg) NO pt2',axLblArgs{:});
 
 
+hFig(end+1) = figure(25);
+hfig = hFig(end);
+tstr = sprintf('nIntra=%d. headAngErr (inc pt2) vs mean px err',nIntra);
+set(hfig,'Name',tstr,'Position',[2561 401 1920 1124]);
+scatter(intra_l2err_dLbl,intra_axisAngRadMag_dLbl/pi*180);
+grid on;
+title(tstr,tiLblArgs{:});
+xlabel('mean err (px)',axLblArgs{:});
+ylabel('angle err (deg) inc pt2',axLblArgs{:});
+lims = axis;
+lims([1 3]) = 0;
+axis(lims);
+
+
+hFig(end+1) = figure(30);
+hfig = hFig(end);
+tstr = sprintf('nIntra=%d. headAngErr vs headAng',nIntra);
+set(hfig,'Name',tstr,'Position',[2561 401 1920 1124]);
+axs = mycreatesubplots(1,2,.1);
+linkaxes(axs);
+
+ax = axs(1);
+axes(ax);
+tstr = sprintf('nIntra=%d. headAngErr vs headAng, inc pt2',nIntra);
+scatter(intra_axAngDegXYZMag,intra_axisAngRadMag_dLbl/pi*180);
+grid on;
+title(tstr,tiLblArgs{:});
+xlabel('head ang mag (deg)',axLblArgs{:});
+ylabel('head ang err (deg) inc pt2',axLblArgs{:});
+
+ax = axs(2);
+axes(ax);
+tstr = sprintf('NO. pt2');
+scatter(intra_axAngDegXYZMag,intra_axisAngRadMag_dLbl_nopt2/pi*180);
+grid on;
+title(tstr,tiLblArgs{:});
+xlabel('head ang mag (deg)',axLblArgs{:});
+ylabel('head ang err (deg) NO pt2',axLblArgs{:});
+
+hFig(end+1) = figure(40);
+hfig = hFig(end);
+tstr = sprintf('nIntra=%d. headAngErr dists',nIntra);
+set(hfig,'Name',tstr,'Position',[2561 401 1920 1124]);
+clf;
+axs = mycreatesubplots(1,2,.1);
+
+ax = axs(1);
+axes(ax);
+tstr = sprintf('intra-pairs head angle error (nIntra=%d)',nIntra);
+hist(intra_axisAngRadMag_dLbl/pi*180);
+grid on;
+title(tstr,tiLblArgs{:});
+xlabel('head ang err (deg)',axLblArgs{:});
+
+ax = axs(2);
+axes(ax);
+tstr = sprintf('NO pt 2');
+hist(intra_axisAngRadMag_dLbl_nopt2/pi*180);
+grid on;
+title(tstr,tiLblArgs{:});
+xlabel('head ang err (deg) NO pt2',axLblArgs{:});
+
+linkaxes(axs);
+
+% get row indices of tGT for various angMag ptiles
+ANGMAGPTILES = linspace(0,100,8);
+nGT = height(tGT);
+angMag = tGT.axAngDegXYZ(:,1);
+[~,idx] = sort(angMag);
+idxidxs = round(ANGMAGPTILES/100*nGT);
+idxidxs = max(1,idxidxs);
+idxidxs = min(nGT,idxidxs);
+fprintf(1,'idxidxs are %s\n',mat2str(idxidxs));
+idxPlot = idx(idxidxs); % plot these rows of tGT
+% get cached images from lbl from rows of tGT
+ppdataMD = lbl.preProcData.MD(:,{'mov' 'frm'});
+ppdataMD.Properties.VariableNames{1} = 'mIdx';
+ppdataMD.mIdx = int32(ppdataMD.mIdx);
+[tf,loc] = tblismember(tGT,ppdataMD,{'mIdx' 'frm'});
+all(tf)
+IGTcropped = lbl.preProcData.I(loc,:);
+
+for ivw=1:2
+  hFig(end+1) = figure(50+ivw);
+  hfig = hFig(end);
+  tstr = sprintf('Lbl montage, angMag ptiles=%s. Vw%d',mat2str(ANGMAGPTILES,3),ivw);
+  set(hfig,'Name',tstr,'Position',[2561 401 1920 1124]);
+  pLblAbsVw = tGT.pLbl(:,[1:5 11:15]+(ivw-1)*5);
+  roiVw = tGT.roi(:,(1:4)+(ivw-1)*4);
+  pLblRoiVw = Shape.p2pROI(pLblAbsVw,roiVw,5);
+  framelbls = arrayfun(@(x)sprintf('angMag=%.3f',x),angMag(idxPlot),'uni',0);
+  Shape.montage(IGTcropped(:,ivw),pLblRoiVw,...
+    'fig',hfig,...
+    'nr',2,'nc',4,...
+    'idxs',idxPlot,...
+    'framelbls',framelbls,...
+    'framelblscolor',[1 1 0],...
+    'titlestr',tstr);
+end
+
+if DOSAVE
+  for i=1:numel(hFig)
+    h = figure(hFig(i));
+    fname = h.Name;
+    hgsave(h,fullfile(SAVEDIR,[fname '.fig']));
+    set(h,'PaperOrientation','landscape','PaperType','arch-d');
+    print(h,'-dpdf',fullfile(SAVEDIR,[fname '.pdf']));  
+    print(h,'-dpng','-r300',fullfile(SAVEDIR,[fname '.png']));   
+    fprintf(1,'Saved %s.\n',fname);
+  end
+end
 %%
 maxStroErr = nanmax([tGT.stroErr3dLbl tGT.stroErr3dTrk],[],2);
 mnStroErr = nanmean([tGT.stroErr3dLbl tGT.stroErr3dTrk],2);
