96ffe76fd04a7362ddcf835abf87434014427b0f
Mayank Home Workstation <mayank@ws.home>
2018-09-13 18:39:43 +0530

    Adding official tf resnet in addition to slim resnet

--- deepnet/PoseUNet_resnet.py
+++ deepnet/PoseUNet_resnet.py
@@ -16,6 +16,7 @@ from tensorflow.contrib.slim.nets import resnet_v1
 import tensorflow.contrib.slim as slim
 from PoseCommon_dataset import conv_relu3
 from tensorflow.contrib.layers import batch_norm
+import resnet_official
 
 class PoseUNet_resnet(PoseUNet.PoseUNet):
 
@@ -40,18 +41,31 @@ class PoseUNet_resnet(PoseUNet.PoseUNet):
             a,b,self.ph['phase_train'], keep_prob=None,
             use_leaky=self.conf.unet_use_leaky)
 
-        with slim.arg_scope(resnet_v1.resnet_arg_scope()):
-            net, end_points = resnet_v1.resnet_v1_50(im,
-                                      global_pool=False, is_training=self.ph['phase_train'])
+
+        if self.resnet_version == 'slim':
+            with slim.arg_scope(resnet_v1.resnet_arg_scope()):
+                net, end_points = resnet_v1.resnet_v1_50(im,
+                                          global_pool=False, is_training=self.ph[
+                                          'phase_train'])
+                l_names = ['conv1', 'block1/unit_2/bottleneck_v1', 'block2/unit_3/bottleneck_v1',
+                           'block3/unit_5/bottleneck_v1', 'block4']
+                down_layers = [end_points['resnet_v1_50/' + x] for x in l_names]
+
+                ex_down_layers = conv(self.inputs[0], 64)
+                down_layers.insert(0, ex_down_layers)
+                n_filts = [32, 64, 64, 128, 256, 512]
+
+        elif self.resnet_version == 'official_tf':
+            mm = resnet_official.Model( resnet_size=50, bottleneck=True, num_classes=17, num_filters=32, kernel_size=7, conv_stride=2, first_pool_size=3, first_pool_stride=2, block_sizes=[3, 4, 6, 3], block_strides=[2, 2, 2, 2], final_size=2048, resnet_version=2, data_format='channels_last',dtype=tf.float32)
+            im = tf.placeholder(tf.float32, [8, 512, 512, 3])
+            resnet_out = mm(im, True)
+            down_layers = mm.layers
+            ex_down_layers = conv(self.inputs[0], 64)
+            down_layers.insert(0, ex_down_layers)
+            n_filts = [32, 64, 64, 128, 256, 512, 1024]
+
 
         with tf.variable_scope(self.net_name):
-
-            l_names = ['conv1', 'block1/unit_2/bottleneck_v1','block2/unit_3/bottleneck_v1', 'block3/unit_5/bottleneck_v1', 'block4']
-            down_layers = [end_points['resnet_v1_50/'+x] for x in l_names]
-            n_filts = [32, 64, 64, 128, 256, 512]
-
-            ex_down_layers =  conv(self.inputs[0], 64)
-            down_layers.insert(0,ex_down_layers)
 
             prev_in = None
             for ndx in reversed(range(len(down_layers))):
@@ -72,19 +86,19 @@ class PoseUNet_resnet(PoseUNet.PoseUNet):
 
                     layers_sz = down_layers[ndx-1].get_shape().as_list()[1:3]
                     with tf.variable_scope('u_{}'.format(ndx)):
-                        # X = CNB.upscale('u_{}'.format(ndx), X, layers_sz)
-                        X_sh = X.get_shape().as_list()
-                        w_mat = np.zeros([4,4,X_sh[-1],X_sh[-1]])
-                        for wndx in range(X_sh[-1]):
-                            w_mat[:,:,wndx,wndx] = 1.
-                        w = tf.get_variable('w', [4, 4, X_sh[-1], X_sh[-1]],initializer=tf.constant_initializer(w_mat))
-                        out_shape = [X_sh[0],layers_sz[0],layers_sz[1],X_sh[-1]]
-                        X = tf.nn.conv2d_transpose(X, w, output_shape=out_shape, strides=[1, 2, 2, 1], padding="SAME")
-                        biases = tf.get_variable('biases', [out_shape[-1]], initializer=tf.constant_initializer(0))
-                        conv_b = X + biases
-
-                        bn = batch_norm(conv_b)
-                        X = tf.nn.relu(bn)
+                        X = CNB.upscale('u_{}'.format(ndx), X, layers_sz)
+                        # X_sh = X.get_shape().as_list()
+                        # w_mat = np.zeros([5,5,X_sh[-1],X_sh[-1]])
+                        # for wndx in range(X_sh[-1]):
+                        #     w_mat[:,:,wndx,wndx] = 1.
+                        # w = tf.get_variable('w', [5, 5, X_sh[-1], X_sh[-1]],initializer=tf.constant_initializer(w_mat))
+                        # out_shape = [X_sh[0],layers_sz[0],layers_sz[1],X_sh[-1]]
+                        # X = tf.nn.conv2d_transpose(X, w, output_shape=out_shape, strides=[1, 2, 2, 1], padding="SAME")
+                        # biases = tf.get_variable('biases', [out_shape[-1]], initializer=tf.constant_initializer(0))
+                        # conv_b = X + biases
+                        #
+                        # bn = batch_norm(conv_b)
+                        # X = tf.nn.relu(bn)
 
                 prev_in = X
 
@@ -104,7 +118,7 @@ class PoseUNet_resnet(PoseUNet.PoseUNet):
         var_list = tf.global_variables(self.net_name)
         for dep_net in self.dep_nets:
             var_list += dep_net.get_var_list()
-        var_list += tf.global_variables('resnet_v1_50')
+        var_list += tf.global_variables('resnet_')
         return var_list
 
 
@@ -114,6 +128,7 @@ class PoseUMDN_resnet(PoseUMDN.PoseUMDN):
         conf.use_pretrained_weights = True
 #        conf.pretrained_weights = '/home/mayank/work/deepcut/pose-tensorflow/models/pretrained/resnet_v1_50.ckpt'
         self.conf = conf
+        self.resnet_source = 'official_tf'
         PoseUMDN.PoseUMDN.__init__(self, conf, name=name)
         self.dep_nets = []
 
@@ -132,8 +147,30 @@ class PoseUMDN_resnet(PoseUMDN.PoseUMDN):
             a,b,self.ph['phase_train'], keep_prob=None,
             use_leaky=self.conf.unet_use_leaky)
 
-        with slim.arg_scope(resnet_v1.resnet_arg_scope()):
-            net, end_points = resnet_v1.resnet_v1_50(im,global_pool=False, is_training=self.ph['phase_train'])
+        if self.resnet_source == 'slim':
+            with slim.arg_scope(resnet_v1.resnet_arg_scope()):
+                net, end_points = resnet_v1.resnet_v1_50(im,
+                                                         global_pool=False, is_training=self.ph[
+                        'phase_train'])
+                l_names = ['conv1', 'block1/unit_2/bottleneck_v1', 'block2/unit_3/bottleneck_v1',
+                           'block3/unit_5/bottleneck_v1', 'block4']
+                down_layers = [end_points['resnet_v1_50/' + x] for x in l_names]
+
+                n_filts = [32, 64, 64, 128, 256, 512]
+
+        elif self.resnet_source == 'official_tf':
+            mm = resnet_official.Model(resnet_size=50, bottleneck=True, num_classes=17, num_filters=64, kernel_size=7,
+                                       conv_stride=2, first_pool_size=3, first_pool_stride=2, block_sizes=[3, 4, 6, 3],
+                                       block_strides=[2, 2, 2, 2], final_size=2048, resnet_version=2,
+                                       data_format='channels_last', dtype=tf.float32)
+            resnet_out = mm(im, True)
+            down_layers = mm.layers
+            net = down_layers[-1]
+            n_filts = [32, 64, 64, 128, 256, 512, 1024]
+
+        # add an extra layer at input resolution.
+        ex_down_layers = conv(im, 64)
+        down_layers.insert(0, ex_down_layers)
 
         X = net
         k = 2
@@ -142,13 +179,6 @@ class PoseUMDN_resnet(PoseUMDN.PoseUMDN):
         n_out = self.conf.n_classes
 
         with tf.variable_scope(self.net_name + '_unet'):
-
-            l_names = ['conv1', 'block1/unit_2/bottleneck_v1','block2/unit_3/bottleneck_v1', 'block3/unit_5/bottleneck_v1', 'block4']
-            down_layers = [end_points['resnet_v1_50/'+x] for x in l_names]
-            n_filts = [32, 64, 64, 128, 256, 512]
-
-            ex_down_layers =  conv(self.inputs[0], 64)
-            down_layers.insert(0,ex_down_layers)
 
             prev_in = None
             for ndx in reversed(range(len(down_layers))):
@@ -195,7 +225,6 @@ class PoseUMDN_resnet(PoseUMDN.PoseUMDN):
             X = tf.add(conv, biases, name = 'unet_pred')
             X_unet = 2*tf.sigmoid(X)-1
 
-        X = net
         with tf.variable_scope(self.net_name):
 
             n_filt_in = X.get_shape().as_list()[3]
@@ -310,7 +339,7 @@ class PoseUMDN_resnet(PoseUMDN.PoseUMDN):
 
     def get_var_list(self):
         var_list = tf.global_variables(self.net_name)
-        var_list += tf.global_variables('resnet_v1_50')
+        var_list += tf.global_variables('resnet_')
         return var_list
 
 
