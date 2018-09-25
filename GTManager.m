a4bdb20e2393a19e3138908eb6d7c89c393d63f5
Allen <allenleetc@gmail.com>
2018-09-25 12:00:25 -0400

    testing bsub tracking w crops. GTManager should not be a singleton.

--- GTManager.m
+++ GTManager.m
@@ -1,10 +1,10 @@
 function varargout = GTManager(varargin)
 % Movie table GUI
 
-% Last Modified by GUIDE v2.5 27-Sep-2017 06:22:13
+% Last Modified by GUIDE v2.5 24-Sep-2018 18:50:06
 
 % Begin initialization code - DO NOT EDIT
-gui_Singleton = 1;
+gui_Singleton = 0;
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
                    'gui_OpeningFcn', @GTManager_OpeningFcn, ...
