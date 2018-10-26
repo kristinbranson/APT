f2142ad02b7b20db484b9ad729dd960a51ccf11c
Kristin Branson <bransonk@janelia.hhmi.org>
2018-10-26 18:25:46 -0400

    Working on getting cropping mode to work.

--- LabelerGUI.m
+++ LabelerGUI.m
@@ -3632,3 +3632,6 @@ function pushbutton_exitcropmode_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton_exitcropmode (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+
+lObj = handles.labelerObj;
+lObj.cropSetCropMode(false);
