aec65443b61c453bd4f4bfc8278972e67c82f70c
Mayank Home Workstation <mayank@ws.home>
2019-01-25 13:43:29 +0530

    Removing pretrained weight download from python

--- deepnet/PoseUNet_resnet.py
+++ deepnet/PoseUNet_resnet.py
@@ -140,20 +140,22 @@ class PoseUMDN_resnet(PoseUMDN.PoseUMDN):
         self.min_dist = 5
 
         #download pretrained weights if they don't exist
-        if self.resnet_source == 'official_tf':
-            url = 'http://download.tensorflow.org/models/official/20181001_resnet/savedmodels/resnet_v2_fp32_savedmodel_NHWC.tar.gz'
-            script_dir = os.path.dirname(os.path.realpath(__file__))
-            wt_dir = os.path.join(script_dir,'pretrained')
-            wt_file = os.path.join(wt_dir,'resnet_v2_fp32_savedmodel_NHWC','1538687283','variables','variables.index')
-            if not os.path.exists(wt_file):
-                print('Downloading pretrained weights..')
-                if not os.path.exists(wt_dir):
-                    os.makedirs(wt_dir)
-                sname, header = urllib.urlretrieve(url)
-                tar = tarfile.open(sname, "r:gz")
-                print('Extracting pretrained weights..')
-                tar.extractall(path=wt_dir)
-            self.pretrained_weights = os.path.join(wt_dir,'resnet_v2_fp32_savedmodel_NHWC','1538687283','variables','variables')
+        # if self.resnet_source == 'official_tf':
+        #     url = 'http://download.tensorflow.org/models/official/20181001_resnet/savedmodels/resnet_v2_fp32_savedmodel_NHWC.tar.gz'
+        #     script_dir = os.path.dirname(os.path.realpath(__file__))
+        #     wt_dir = os.path.join(script_dir,'pretrained')
+        #     wt_file = os.path.join(wt_dir,'resnet_v2_fp32_savedmodel_NHWC','1538687283','variables','variables.index')
+        #     if not os.path.exists(wt_file):
+        #         print('Downloading pretrained weights..')
+        #         if not os.path.exists(wt_dir):
+        #             os.makedirs(wt_dir)
+        #         sname, header = urllib.urlretrieve(url)
+        #         tar = tarfile.open(sname, "r:gz")
+        #         print('Extracting pretrained weights..')
+        #         tar.extractall(path=wt_dir)
+        script_dir = os.path.dirname(os.path.realpath(__file__))
+        wt_dir = os.path.join(script_dir,'pretrained')
+        self.pretrained_weights = os.path.join(wt_dir,'resnet_v2_fp32_savedmodel_NHWC','1538687283','variables','variables')
 
 
     def create_network(self):
