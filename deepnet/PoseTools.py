b25df0c123420d558f3b1c99830bef351141d0f1
Allen Lee <allenleetc@gmail.com>
2019-03-15 23:11:33 -0400

    revert debug prints

--- deepnet/PoseTools.py
+++ deepnet/PoseTools.py
@@ -737,8 +737,6 @@ def get_vars(vstr):
 
 
 def compare_conf(curconf, oldconf):
-    print (dir(curconf))
-    print (dir(oldconf))
     ff = list(set(dir(curconf))|set(dir(oldconf)))
     for f in ff:
         if f[0:2] == '__' or f[0:3] == 'get':
