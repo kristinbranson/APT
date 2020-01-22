770a0d1ddf2422dcf38f0abb1703ffd4f8235cc9
Allen Lee <allenleetc@gmail.com>
2020-01-22 13:45:25 -0500

    update SH megaproj

--- anls/gt2sh/nbAddLabelsToSHMegaProj20200121.m
+++ anls/gt2sh/nbAddLabelsToSHMegaProj20200121.m
@@ -103,12 +103,55 @@ createProject(projFull{2},stuff.mfa,...
   'outfile',fullfile(pwd,'sh_trn5017_20200121.lbl'),...
   'diaryfile',fullfile(pwd,'dry_sh_trn5017_20200121.txt'));
 
+%% compare to prev
+
+s1 = load('sh_trn5017_20200121_stripped.lbl','-mat');
+s0 = load('../../blapt/experiments/data/sh_trn4992_gtcomplete_cacheddata_updatedAndPpdbManuallyCopied20190402_dlstripped.lbl','-mat');
+% mov causes trouble with reads
+mov = '/groups/huston/hustonlab/flp-chrimson_experiments/fly_380_to_fly_389_12_9_16_SS02323_and_SS002323POsilenced/SS002323CsChrimson_lexA48A07Kir/fly382/C001H001S0001/C001H001S0001_c.avi';
+imov = find(strcmp(s0.movieFilesAll(:,1),mov));
 %%
-
-
-
-
-
-
+isequaln(s0.movieFilesAll,s1.movieFilesAll(1:size(s0.movieFilesAll,1),:))
+FLDS = {
+  'preProcData_I'
+  'preProcData_MD_frm'
+  'preProcData_MD_iTgt'
+  'preProcData_MD_mov'
+  'preProcData_MD_nNborMask'
+  'preProcData_MD_roi'
+  'preProcData_MD_tfocc'
+  'preProcData_P'
+  };
+for f=FLDS(:)',f=f{1};
+  tf = isequaln(s0.(f),s1.(f)(1:4992,:));
+  fprintf(1,'%s: %d\n',f,tf);
+end
+%%
+idx = find(s0.preProcData_MD_mov==imov)
+frms = s0.preProcData_MD_frm(idx);
+nfrm = numel(frms);
+roisvw0 = s0.preProcData_MD_roi(idx,1:4)
+[rf0,nf0,fid0] = get_readframe_fcn(mov);
+[rf1,nf1,fid1] = get_readframe_fcn(mov,'preload',true);
+I0 = arrayfun(@(x)rf0(x),frms,'uni',0);
+I1 = arrayfun(@(x)rf1(x),frms,'uni',0);
+%%
+xlo = roisvw0(1,1);
+xhi = roisvw0(1,2);
+ylo = roisvw0(1,3);
+yhi = roisvw0(1,4);
+I0crop = cellfun(@(x)x(ylo:yhi,xlo:xhi,1),I0,'uni',0);
+I1crop = cellfun(@(x)x(ylo:yhi,xlo:xhi,1),I1,'uni',0);
+I0stripped = s0.preProcData_I(idx,1);
+I1stripped = s1.preProcData_I(idx,1);
+isequal(I0crop,I1stripped)
+isequal(I1crop,I1stripped)
+%%
+d = nan(size(I0crop));
+for i=1:numel(I0crop)
+  tmp = I0stripped{i}-I1stripped{i};
+  d(i) = max(abs(tmp(:)));
+end
+%%
 ld = cellfun(@loadLbl,projFull);
 s = cellfun(@(x)load(x,'-mat'),stripFull);
