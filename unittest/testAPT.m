c19215d2e6fb1da6460148672a1b048983353dd6
Allen Lee <al@leapscientific.com>
2020-03-10 17:16:25 -0400

    call APT meth to get cacheroot

--- unittest/testAPT.m
+++ unittest/testAPT.m
@@ -57,14 +57,15 @@ classdef testAPT < handle
     end
     
     function cdir = get_cache_dir()
-      if ispc
-        userDir = winqueryreg('HKEY_CURRENT_USER',...
-          ['Software\Microsoft\Windows\CurrentVersion\' ...
-          'Explorer\Shell Folders'],'Personal');
-      else
-        userDir = char(java.lang.System.getProperty('user.home'));
-      end
-      cdir = fullfile(userDir,'.apt');
+      cdir = APT.getdlcacheroot;
+%       if ispc
+%         userDir = winqueryreg('HKEY_CURRENT_USER',...
+%           ['Software\Microsoft\Windows\CurrentVersion\' ...
+%           'Explorer\Shell Folders'],'Personal');
+%       else
+%         userDir = char(java.lang.System.getProperty('user.home'));
+%       end
+%       cdir = fullfile(userDir,'.apt');
     end
     
   end
