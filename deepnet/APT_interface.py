be3b41305dd4b88e5aebec3ef00a4e533acf7e22
Kristin Branson <bransonk@janelia.hhmi.org>
2020-12-21 14:02:27 -0500

    Minor change to convert_to_mat_trk for if you don't have a configuration.

--- deepnet/APT_interface.py
+++ deepnet/APT_interface.py
@@ -2123,14 +2123,14 @@ def convert_to_mat_trk(in_pred, conf, start, end, trx_ids, has_trx_file=None):
         pred_locs = pred_locs[..., 0]
     return pred_locs
 
-def write_trk(out_file, pred_locs_in, extra_dict, start, end, trx_ids, conf, info, mov_file):
+def write_trk(out_file, pred_locs_in, extra_dict, start, end, trx_ids, conf, info, mov_file, has_trx_file=None):
     '''
     pred_locs is the predicted locations of size
     n_frames in the movie x n_Trx x n_body_parts x 2
     n_done is the number of frames that have been tracked.
     everything should be 0-indexed
     '''
-    pred_locs = convert_to_mat_trk(pred_locs_in, conf, start, end, trx_ids)
+    pred_locs = convert_to_mat_trk(pred_locs_in, conf, start, end, trx_ids, has_trx_file)
     pred_locs = to_mat(pred_locs)
 
     tgt = to_mat(np.array(trx_ids))  # target animals that have been tracked.
@@ -2151,7 +2151,7 @@ def write_trk(out_file, pred_locs_in, extra_dict, start, end, trx_ids, conf, inf
                 'pTrkFrm': tracked,
                 'trkInfo': info}
     for k in extra_dict.keys():
-        tmp = convert_to_mat_trk(extra_dict[k], conf, start, end, trx_ids)
+        tmp = convert_to_mat_trk(extra_dict[k], conf, start, end, trx_ids, has_trx_file)
         if k.startswith('locs_'):
             tmp = to_mat(tmp)
         out_dict['pTrk' + k] = tmp
