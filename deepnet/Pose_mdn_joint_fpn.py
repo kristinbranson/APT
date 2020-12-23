37d9177fe76dc35002e1cf0ee5c568b18357f431
Mayank Kabra <kabram@janelia.hhmi.org>
2020-12-23 03:29:34 -0500

    Adding images for dataset documentation

--- deepnet/Pose_mdn_joint_fpn.py
+++ deepnet/Pose_mdn_joint_fpn.py
@@ -4,4 +4,5 @@ class Pose_mdn_joint_fpn(Pose_mdn_joint):
 
     def __init__(self, conf, **kwargs):
         conf.mdn_joint_use_fpn = True
+        conf.mdn_use_unet_loss = False
         Pose_mdn_joint.__init__(self,conf,**kwargs)
