0e27eea25ee1d02a296703b622dfd12dcf0a96e6
Kristin Branson <bransonk@janelia.hhmi.org>
2020-12-24 14:13:59 -0500

    Fixed bug in Labels.toarray -- input ntgts was not being set correctly when loading multitarget tracking. There was also a giant slow loop that was slowing down access to trajectories, vectorized.

--- matlab/Labels.m
+++ matlab/Labels.m
@@ -341,32 +341,36 @@ classdef Labels
         'ntgt',[] ... % num tgts "
         );
       if isempty(nfrm)
-        if isempty(s.frm)
-          nfrm = 1;
-        else
-          nfrm = max(s.frm);
-        end
+        nfrm = 1;
       end
+      if ~isempty(s.frm),
+        nfrm = max(nfrm,max(s.frm));
+      end
+      % KB 20201224: ntgt was not being set right
       if isempty(ntgt)
-        if isempty(s.tgt)
-          ntgt = 1;
-        else
-          ntgt = max(s.tgt);
-        end
+        ntgt = 1;
+      end
+      if ~isempty(s.tgt),
+        ntgt = max(ntgt,max(s.tgt));
       end
       
       lpos = nan(s.npts,2,nfrm,ntgt);
       lposTS = -inf(s.npts,nfrm,ntgt);
       lpostag = false(s.npts,nfrm,ntgt);
 
-      n = numel(s.frm);
-      for i=1:n
-        f = s.frm(i);
-        itgt = s.tgt(i);
-        lpos(:,:,f,itgt) = reshape(s.p(:,i),s.npts,2);
-        lposTS(:,f,itgt) = s.ts(:,i);
-        lpostag(:,f,itgt) = s.occ(:,i);
-      end
+      % KB 20201224 this loop was slow!
+      idx = sub2ind([nfrm,ntgt],s.frm,s.tgt);
+      lpos(:,:,idx) = reshape(s.p,[s.npts,2,numel(s.frm)]);
+      lposTS(:,idx) = s.ts;
+      lpostag(:,idx) = s.occ;
+%       n = numel(s.frm);
+%       for i=1:n
+%         f = s.frm(i);
+%         itgt = s.tgt(i);
+%         lpos(:,:,f,itgt) = reshape(s.p(:,i),s.npts,2);
+%         lposTS(:,f,itgt) = s.ts(:,i);
+%         lpostag(:,f,itgt) = s.occ(:,i);
+%       end
     end
     function s = fromarray(lpos,varargin)
       % s = fromarray(lpos,'lposTS',lposTS,'lpostag',lpostag)
