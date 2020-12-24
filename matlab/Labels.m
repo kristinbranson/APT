0be3e93eb99fe2d3b347ec9fcd1395bd6333419a
Kristin Branson <bransonk@janelia.hhmi.org>
2020-12-24 16:54:08 -0500

    Fixed more bugs in trk importing -- frms are stored as uint32s, and negative offset was computed from this, needed to convert to double.

--- matlab/Labels.m
+++ matlab/Labels.m
@@ -448,10 +448,11 @@ classdef Labels
       xfcn = @(p)nanmean(p(1:s.npts,:),1);
       yfcn = @(p)nanmean(p(s.npts+1:2*s.npts,:),1);
       
+      
       for jtgt=1:ntgts        
         iTgt = tgtsUn(jtgt);
         tf = s.tgt==iTgt;
-        frms = s.frm(tf);
+        frms = double(s.frm(tf)); % KB 20201224 - doesn't work if uint32, off should be negative
         
         f0 = min(frms);
         f1 = max(frms);
