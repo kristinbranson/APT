32dd86213ceca740e5616e483beb6fb2efb79704
Allen Lee <allenleetc@gmail.com>
2021-11-15 17:02:23 -0500

    reconstruct method for CalRigNPairwiseCalibrated

--- matlab/user/CalRigNPairwiseCalibrated.m
+++ matlab/user/CalRigNPairwiseCalibrated.m
@@ -53,21 +53,21 @@ classdef CalRigNPairwiseCalibrated < CalRig & matlab.mixin.Copyable
   methods
 
     % Helper. Get stereo crig and map "absolute" cam indices to left/right
-    function [crigstro,ivw1stro,ivw2stro] = getStroCalRig(obj,iView1,iView2)
+    function [crigstro,ivwLstro,ivwRstro] = getStroCalRig(obj,iView1,iView2)
       % iView1/2: view indices
       %
       % crigstro: calrig object for iview1,2 pair
-      % ivw1stro: 1 if iView1 maps to "left", 2 if "right"
-      % ivw2stro: "    iView2  "
+      % ivwLstro: 1 if iView1 maps to "left", 2 if "right"
+      % ivwRstro: "    iView2  "
       
       if iView1<iView2
         crigstro = obj.crigStros{iView1,iView2};
-        ivw1stro = 1;
-        ivw2stro = 2;
+        ivwLstro = 1;
+        ivwRstro = 2;
       elseif iView2<iView1
         crigstro = obj.crigStros{iView2,iView1};
-        ivw1stro = 2;
-        ivw2stro = 1;
+        ivwLstro = 2;
+        ivwRstro = 1;
       else
         assert(false);
       end      
@@ -75,14 +75,51 @@ classdef CalRigNPairwiseCalibrated < CalRig & matlab.mixin.Copyable
         
     %CalRig
     function [xEPL,yEPL] = computeEpiPolarLine(obj,iView1,xy1,iViewEpi,imroi)
-      [crigstro,ivw1stro,ivw2stro] = obj.getStroCalRig(iView1,iViewEpi);
-      [xEPL,yEPL] = crigstro.computeEpiPolarLine(ivw1stro,xy1,ivw2stro,imroi);      
+      [crigstro,ivwLstro,ivwRstro] = obj.getStroCalRig(iView1,iViewEpi);
+      [xEPL,yEPL] = crigstro.computeEpiPolarLine(ivwLstro,xy1,ivwRstro,imroi);      
     end
     
     %CalRig
     function [xRCT,yRCT] = reconstruct(obj,iView1,xy1,iView2,xy2,iViewRct)
-      xRCT = nan;
-      yRCT = nan;
+      
+      assert(numel(xy1)==2);
+      assert(numel(xy2)==2);
+      
+      [crigTri,ivwLTri,ivwRTri] = obj.getStroCalRig(iView1,iView2);
+      xy = cat(3,xy1(:),xy2(:));
+      idxTri = [ivwLTri ivwRTri]; % [1 2] if iView1/2 are l/r; else [2 1] 
+      xy = xy(:,:,idxTri);
+
+      iviewsTriangulate = [iView1 iView2];
+      iviewLTriangulate = iviewsTriangulate(idxTri(1));
+      iviewRTriangulate = iviewsTriangulate(idxTri(2));
+
+      X = crigTri.triangulate(xy); % X in coord sys of crigstro 'L', or iviewLTriangulate
+      % Triangulate 2d points into 3d position
+      % xy: [2xnxnviews] 2d image points
+      % X: [3xn] reconstructed 3d points. coord sys may depend on concrete
+      %   subclass. (typically, coord sys of camera 1.)  
+      ylTri = crigTri.x2y(crigTri.project(X,'L'),'L');
+      XrTri = crigTri.camxform(X,'LR');
+      yrTri = crigTri.x2y(crigTri.project(XrTri,'R'),'R');
+      fprintf(1,'RP pt. View %d: %s. View %d: %s.\n',iviewLTriangulate,...
+        mat2str(round(ylTri(end:-1:1))),iviewRTriangulate,mat2str(round(yrTri(end:-1:1))));
+      
+      [crigRC,ivwLRC] = obj.getStroCalRig(iviewLTriangulate,iViewRct);
+      CAMXFORMS = {'LR' 'RL'};
+      camxform = CAMXFORMS{ivwLRC}; % if ivw1RC==1, translate l->r. else translate r->l.
+      Xrc = crigRC.camxform(X,camxform); % transform from iviewLTriangulate->iViewRct
+      camRCT = camxform(2);
+      y = crigRC.x2y(crigRC.project(Xrc,camRCT),camRCT);
+      
+      [crigRC2,ivwLRC2] = obj.getStroCalRig(iviewRTriangulate,iViewRct);
+      camxform = CAMXFORMS{ivwLRC2};
+      Xrc2 = crigRC2.camxform(XrTri,camxform);
+      camRCT = camxform(2);
+      y2 = crigRC2.x2y(crigRC2.project(Xrc2,camRCT),camRCT);      
+      
+      xRCT = [y([2 2]) y2(2)];
+      yRCT = [y([1 1]) y2(1)];
     end
         
   end
