5a6e6fb10194cc546d231499a0c4490b482dc8ca
Adam L. Taylor <taylora@janelia.hhmi.org>
2023-09-29 17:40:45 -0400

    Got rid of vendored mmpose 0.29.

--- deepnet/mmpose/datasets/datasets/gesture/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .nvgesture_dataset import NVGestureDataset
-
-__all__ = ['NVGestureDataset']
--- deepnet/mmpose/models/necks/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .fpn import FPN
-from .gap_neck import GlobalAveragePooling
-from .posewarper_neck import PoseWarperNeck
-from .tcformer_mta_neck import MTA
-
-__all__ = ['GlobalAveragePooling', 'PoseWarperNeck', 'FPN', 'MTA']
