98bac60a836938d20570f9baec51b6f92e872e40
Adam L. Taylor <taylora@janelia.hhmi.org>
2024-02-07 20:49:32 -0500

    track-wrapper: Updated test

--- deepnet/test_apt_track_wrapper_conda.py
+++ deepnet/test_apt_track_wrapper_conda.py
@@ -5,8 +5,8 @@ SCRIPT_FILE_PATH = os.path.realpath(__file__)
 SCRIPT_FOLDER_PATH = os.path.dirname(SCRIPT_FILE_PATH)
 
 TEST_SCRIPT_PATH = os.path.join(SCRIPT_FOLDER_PATH, 'apt-track')
-LABEL_FILE_PATH = '/groups/branson/bransonlab/taylora/apt/four-points/four-points-testing-2024-01-26-multianimal-branch.lbl'
+LABEL_FILE_PATH = '/groups/branson/bransonlab/taylora/apt/four-points/four-points-testing-2024-02-07-track-wrapper-branch.lbl'
 VIDEO_FILE_PATH = '/groups/branson/bransonlab/roian/apt_testing/files_for_working_with_apt/190530_vocpbm164564_m164564_odor_m164301_f0_164992.mjpg'
-OUTPUT_FILE_PATH = './apt-track-test.trk'
+OUTPUT_FILE_PATH = './apt-track-test-from-script-3.trk'
 
 apt_track_wrapper.main(['-conda', 'apt_20230427_tf211_pytorch113_ampere', '-lbl_file', LABEL_FILE_PATH, '-mov', VIDEO_FILE_PATH, '-start_frame', '1', '-end_frame', '200', '-out', OUTPUT_FILE_PATH])
