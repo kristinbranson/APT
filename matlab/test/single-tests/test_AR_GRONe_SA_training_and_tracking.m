e4494aeeef4cbe9a17be14de6fd8b42f26f24e7f
Adam L. Taylor <taylora@janelia.hhmi.org>
2025-02-25 17:13:49 -0500

    fixing-bugs-on-windows: Renamed one test

--- matlab/test/single-tests/test_AR_GRONe_SA_training_and_tracking.m
+++ matlab/test/single-tests/test_AR_GRONe_SA_training_and_tracking.m
@@ -1,5 +1,5 @@
-function test_AR_GRONe_SA()
-  [~, unittest_dir_path,replace_path] = get_test_project_paths() ;
+function test_AR_GRONe_SA_training_and_tracking()
+  [~, unittest_dir_path, replace_path] = get_test_project_paths() ;
   project_file_path = fullfile(unittest_dir_path, 'multitarget_bubble_training_20210523_allGT_AR_MAAPT_grone2_UT_resaved_2.lbl') ;  
   backend_params = janelia_bsub_backend_params() ;
   backend = fif(ispc(), 'docker', 'bsub') ;      
