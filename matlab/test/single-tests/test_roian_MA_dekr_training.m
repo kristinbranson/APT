240b4d6e14ff1a43d60adfc63dae51f636a7258e
Adam L. Taylor <taylora@janelia.hhmi.org>
2025-03-02 16:55:58 -0500

    fixing-bugs-on-windows: Fixed issue with test_roian_MA_dekr_training.m

--- matlab/test/single-tests/test_roian_MA_dekr_training.m
+++ matlab/test/single-tests/test_roian_MA_dekr_training.m
@@ -2,7 +2,7 @@ function test_roian_MA_dekr_training()
   [~, unittest_dir_path] = get_test_project_paths() ;
   project_file_path = fullfile(unittest_dir_path, 'four-points-testing-2024-11-19-with-rois-added-and-fewer-smaller-movies-mmpose1.lbl') ;  
   backend_params = janelia_bsub_backend_params() ;
-  backend = fif(ispc(), 'docker', 'bsub') ;      
+  backend = 'docker' ;  % project is only set up with correct env/image for docker backend
   tester = LabelerProjectTester(project_file_path) ;  
   oc = onCleanup(@()(delete(tester))) ;
   tester.test_training('algo_name','multi_dekr', ...
