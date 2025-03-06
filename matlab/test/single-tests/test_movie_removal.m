72664c4bf13b94113e8c4787f9e92a8509e61989
Adam L. Taylor <taylora@janelia.hhmi.org>
2025-03-06 18:28:54 -0500

    fixing-bugs-on-windows: Fixed issue with test_movie_removal() on Windows

--- matlab/test/single-tests/test_movie_removal.m
+++ matlab/test/single-tests/test_movie_removal.m
@@ -1,7 +1,18 @@
 function test_movie_removal()
+  linux_project_file_path = '/groups/branson/bransonlab/apt/unittest/four-points-testing-2024-11-19-with-gt-and-rois-added.lbl' ;
+  if ispc()
+    project_file_path = strrep(linux_project_file_path, '/groups/branson/bransonlab', 'Z:') ;
+    replace_path = { '/groups/branson/bransonlab', 'Z:' } ;
+  else
+    project_file_path = linux_project_file_path ;
+    replace_path = [] ;
+  end
+  
   [labeler, controller] = ...
-    StartAPT('projfile', '/groups/branson/bransonlab/apt/unittest/four-points-testing-2024-11-19-with-gt-and-rois-added.lbl');
+    StartAPT('projfile', project_file_path, ...
+             'replace_path', replace_path) ;
   cleaner = onCleanup(@()(delete(controller))) ;  % this will delete labeler too
+  cleaner2 = onCleanup(@()(delete(labeler))) ;  % but just to be sure
 
   originalMovieCount = numel(labeler.movieFilesAll) ;
 
