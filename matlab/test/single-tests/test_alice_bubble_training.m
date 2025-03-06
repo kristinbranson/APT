e06ed6f271ec51348c8c9b2b443d288f291b2ba9
Adam L. Taylor <taylora@janelia.hhmi.org>
2025-03-06 18:14:00 -0500

    fixing-bugs-on-windows: Fix for test_alice_bubble_training() on Windows

--- matlab/test/single-tests/test_alice_bubble_training.m
+++ matlab/test/single-tests/test_alice_bubble_training.m
@@ -1,7 +1,7 @@
 function test_alice_bubble_training()
   % Test training for alice project
   [~, unittest_dir_path, replace_path] = get_test_project_paths() ;
-  project_file_path = fullfile(unittest_dir_path, 'alice/multitarget_bubble_expandedbehavior_20180425_allGT_MK_MDN04182019_updated_20250304.lbl') ;
+  project_file_path = fullfile(unittest_dir_path, 'alice/multitarget_bubble_expandedbehavior_20180425_allGT_MK_MDN04182019_updated_20250306.lbl') ;
   training_params = struct('dlc_override_dlsteps', {true}) ;  % scalar struct
   tester = LabelerProjectTester(project_file_path, 'replace_path', replace_path) ;
   oc = onCleanup(@()(delete(tester))) ;
