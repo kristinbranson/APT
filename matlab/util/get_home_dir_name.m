c1196d86b205dbd8b824b3aac298706e798a6bf0
Adam L. Taylor <taylora@janelia.hhmi.org>
2025-03-24 22:40:57 -0400

    fixing-bugs-on-windows: Added check for HOME envar on Windwos in get_home_dir_name()

--- matlab/util/get_home_dir_name.m
+++ matlab/util/get_home_dir_name.m
@@ -1,11 +1,16 @@
 function result = get_home_dir_name()
 
 if ispc() ,
-  home_drive = getenv('HOMEDRIVE') ;
-  home_path = getenv('HOMEPATH') ;  % will have backslashes as path separators
-  result = horzcat(home_drive, home_path) ;
+  home = getenv('HOME') ;
+  if ~isempty(home)
+    result = getenv('HOME') ;
+  else
+    home_drive = getenv('HOMEDRIVE') ;
+    home_path = getenv('HOMEPATH') ;  % will have backslashes as path separators
+    result = horzcat(home_drive, home_path) ;
+  end
 else
   result = getenv('HOME') ;
-end  
-
 end
+
+end  % function
