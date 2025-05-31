1c4644e00d1d93b01a1f7a06c8105a1bee45fb53
Adam L. Taylor <taylora@janelia.hhmi.org>
2025-05-30 23:54:38 -0400

    recustom: Cherry-pick of MK MA GT fix

--- matlab/Labeler.m
+++ matlab/Labeler.m
@@ -9260,7 +9260,13 @@ classdef Labeler < handle
         TargetSetVariable.AllTgts);    
       tblMFTLbld = mfts.getMFTable(obj);
       
-      [tfSuggAnyLbl,loc] = tblismember(tblMFTSugg,tblMFTLbld,MFTable.FLDSID);
+      % [tfSuggAnyLbl,loc] = tblismember(tblMFTSugg,tblMFTLbld,MFTable.FLDSID);
+      mftflds = MFTable.FLDSID;
+      if obj.maIsMA  
+        % remove tgt field for multi-animal projects
+        mftflds(strcmp(mftflds,'iTgt')) = [];
+      end
+      [tfSuggAnyLbl,loc] = tblismember(tblMFTSugg,tblMFTLbld,mftflds);
 
       % tblMFTLbld includes rows where any pt/coord is labeled;
       % obj.gtSuggMFTableLbled is only true if all pts/coords labeled 
