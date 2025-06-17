13df38e615a32a850cec10db5afa7687317bcabc
Kristin Branson <bransonk@janelia.hhmi.org>
2025-06-17 09:56:20 -0400

    Finished rearranging View menu
    The reordering of menu items no longer seems to work in Matlab 2023b, and seems to be unsupported, short of deleting the menu items and re-adding them.
    I put in a warning in the helper functions for reordering menu items, and will go through and try to remove all calls to them.

--- matlab/misc/menuReorder.m
+++ matlab/misc/menuReorder.m
@@ -4,12 +4,17 @@ function menuReorder(hParent,tags)
 % hParent: scalar parent menu handle
 % tags: cellstr, some permutation of {hParent.Children.Tag}'
 
+warning('Obsolete. You do not seem to be able to change the order of submenus in Matlab any more.');
+
 tag0 = {hParent.Children.Tag}';
 [ism,loc] = ismember(tags,tag0);
 if ~all(ism),
   error('Missing child tag: %s\n',tags{~ism});
 end
-rest = setdiff(1:numel(tag0),loc);
+rest = find(~ismember(tag0,tags));
 loc = [loc(:);rest(:)];
 
 hParent.Children = hParent.Children(loc(end:-1:1));
+for i = 1:numel(hParent.Children),
+  hParent.Children(i).Position = i;
+end
