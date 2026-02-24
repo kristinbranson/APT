9a38a98089d1d980920ed626ae25516990db7105
Adam L. Taylor <taylora@janelia.hhmi.org>
2026-02-23 21:37:08 -0500

    mc3: Add PersistedPrevAxesTargetSpec class for persisted subset of prev-axes spec

---
+++ matlab/PersistedPrevAxesTargetSpec.m
@@ -0,0 +1,74 @@
+classdef PersistedPrevAxesTargetSpec
+  % Value class holding just the parts of PrevAxesTargetSpec that are persisted:
+  % iMov, frm, iTgt, gtmode, dxlim, and dylim.
+  %
+  % Every PersistedPrevAxesTargetSpec in existence is fully valid: the constructor
+  % takes a PrevAxesTargetSpec, a single struct, or name-value pairs, and
+  % asserts isValid() before returning.  The "unset" state is represented by
+  % [] rather than an invalid object.
+
+  properties (SetAccess=immutable)
+    % Persisted fields
+    iMov  % scalar positive integer movie index
+    frm  % scalar positive integer frame number
+    iTgt  % scalar positive integer target index
+    gtmode = false  % scalar logical, whether in GT mode
+    dxlim = [0 0]  % adjustment to xlim as a result of user panning/zooming the sidekick axes, a 2-el row vec
+    dylim = [0 0]  % adjustment to ylim as a result of user panning/zooming the sidekick axes, a 2-el row vec
+  end
+
+  methods
+    function obj = PersistedPrevAxesTargetSpec(varargin)
+      % Construct from a PrevAxesTargetSpec, a single struct, or name-value pairs.
+      % Asserts validity before returning.
+      if nargin == 1 && isa(varargin{1}, 'PrevAxesTargetSpec')
+        source = varargin{1} ;
+        obj.iMov = source.iMov ;
+        obj.frm = source.frm ;
+        obj.iTgt = source.iTgt ;
+        obj.gtmode = source.gtmode ;
+        obj.dxlim = source.dxlim ;
+        obj.dylim = source.dylim ;
+      else
+        if nargin == 1 && isstruct(varargin{1})
+          props = varargin{1} ;
+        else
+          props = struct(varargin{:}) ;
+        end
+        for fieldName = fieldnames(props)'
+          obj.(fieldName{1}) = props.(fieldName{1}) ;
+        end
+      end
+      isValid = isScalarFiniteNonneg(obj.iMov) && ...
+                isScalarFiniteNonneg(obj.frm) && ...
+                isScalarFiniteNonneg(obj.iTgt) && ...
+                isscalar(obj.gtmode) && islogical(obj.gtmode) && ...
+                ~isempty(obj.dxlim) && ...
+                ~isempty(obj.dylim) ;
+      assert(isValid, ...
+             'PersistedPrevAxesTargetSpec: constructed object is not valid.  All required fields must be set.') ;
+    end  % function
+
+    function s = toStruct(obj)
+      % Serialize identity fields + dxlim/dylim to a struct for saving to disk.
+      s = struct('iMov', obj.iMov, 'frm', obj.frm, 'iTgt', obj.iTgt, 'gtmode', obj.gtmode, ...
+                 'dxlim', obj.dxlim, 'dylim', obj.dylim);
+    end  % function
+  end  % methods
+
+  methods (Static)
+    function result = setprop(obj, varargin)
+      % Make a new, independent, object of the class by replacing some field names
+      % Usage: newObj = setprop(obj, prop1, val1, prop2, val2, ...)
+      assert(mod(numel(varargin), 2) == 0, 'setprop:badArgs', 'Arguments must be property-value pairs') ;
+      oldPairs = obj.toStruct() ;
+      newPairs = struct(varargin{:}) ;
+      mergedPairs = oldPairs ;
+      for fieldName = fieldnames(newPairs)'
+        mergedPairs.(fieldName{1}) = newPairs.(fieldName{1});
+      end
+      result = PersistedPrevAxesTargetSpec(mergedPairs) ;
+    end  % function
+
+  end  % methods
+end  % classdef
