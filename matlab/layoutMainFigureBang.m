0fec86502ce34c8909d9aed01da199c101d7114f
Adam L. Taylor <taylora@janelia.hhmi.org>
2026-03-31 18:40:14 -0400

    mc4: Changed min figure layout size

--- matlab/layoutMainFigureBang.m
+++ matlab/layoutMainFigureBang.m
@@ -4,8 +4,8 @@ function layoutMainFigureBang(hFig)
 
 handles = guihandles(hFig) ;
 figPos = hFig.Position ;
-figW = max(figPos(3), 800) ;
-figH = max(figPos(4), 600) ;
+figW = max(figPos(3), 1534) ;
+figH = max(figPos(4), 1062) ;
 
 % -- Derived sizes for the main panel --
 panelW = figW - 460 ;   % left edge at 450, right margin 10
