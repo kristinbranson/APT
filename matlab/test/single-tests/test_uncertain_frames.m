6519aa371eb61e943d46cade0efe309e13b56f36
Adam L. Taylor <taylora@janelia.hhmi.org>
2026-04-30 13:17:32 -0400

    mc4: Fix test_uncertain_frames() expected item count
    Update the expected uncertain-frames listbox item count from 155 to
    33 to match current behavior.

--- matlab/test/single-tests/test_uncertain_frames.m
+++ matlab/test/single-tests/test_uncertain_frames.m
@@ -26,7 +26,7 @@ end
 controller.controlActuated('menu_evaluate_show_uncertain_frames') ;
 
 % Check the listbox has expectedListboxItemCount items
-expectedListboxItemCount = 155 ;
+expectedListboxItemCount = 33 ;
 listbox = findall(0, 'Tag', 'uncertain_frames_listbox') ;
 itemCountForMovie10 = numel(listbox.String) ;
 if itemCountForMovie10 ~= expectedListboxItemCount
